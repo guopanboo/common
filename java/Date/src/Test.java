c9da41a137e9d1b143f5f8933202a76cecb8882d
guopanbo <guopanbo@192.168.1.77>
2017-02-13 14:28:33 +0800

    1

--- java/Date/src/Test.java
+++
@@ -1,8 +0,0 @@
-
-public class Test {
-	public static void main(String[] args) {
-		//System.out.println(DateUtil.getStrByDate(DateUtil.getDaysAgoBegin(50), true));
-		//System.out.println(DateUtil.getStrByDate(new Date(), "HH:ss"));
-		System.out.println(DateUtil.getWeekByDate(DateUtil.getDaysAgoBegin(0)));
-	}
-}
